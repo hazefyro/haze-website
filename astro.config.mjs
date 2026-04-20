39a1b40de014c05aa98f56e19c38591c5918c9e2
haze <92948735+hazetwo@users.noreply.github.com>
2026-04-20 13:54:32 +0200

    astro utils setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,31 @@
 // @ts-check
+import { defineConfig, fontProviders } from "astro/config";
+
+import sitemap from "@astrojs/sitemap";
+
 import cloudflare from "@astrojs/cloudflare";
 
 
 // https://astro.build/config
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  fonts: [
+    {
+      provider: fontProviders.fontsource(),
+      name: "Roboto",
+      cssVariable: "--font-roboto",
+      weights: [400, 600],
+      styles: ["normal"],
+      subsets: ["latin", "latin-ext"],
+    },
+  ],
+  i18n: {
+    locales: ["en", "pl"],
+    defaultLocale: "en",
+  },
+  integrations: [sitemap(), react()],
   adapter: cloudflare(),
   site: "https://haze.fyro.dev",
+});
